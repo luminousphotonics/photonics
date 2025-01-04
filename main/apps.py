33ba3f93d233fc1ada23aba3dbdb16c60fb08ef1
Austin Rouse <austin.rouse@ledcultivation.com>
2025-01-04 11:37:55 -0800

    Finalize deployment configurations with correct Procfile and settings

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,7 @@
+# /Users/austinrouse/photonics/main/apps.py
+
 from django.apps import AppConfig
 
-
 class MainConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "main"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'main'
--- photonics/wsgi.py
+++ photonics/wsgi.py
@@ -4,13 +4,12 @@ WSGI config for photonics project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "photonics.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photonics.settings')
 
 application = get_wsgi_application()

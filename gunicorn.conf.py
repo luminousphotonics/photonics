1bf39d41a059b5b556daf8c864bb8d5a83e875bb
Austin Rouse <austin.rouse@ledcultivation.com>
2025-01-07 03:48:39 -0800

    Add gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,4 @@
+# gunicorn.conf.py
+timeout = 300  # Timeout in seconds (adjust as needed)
+workers = 3    # Optional: Set the number of worker processes
+# Add any other Gunicorn settings you require

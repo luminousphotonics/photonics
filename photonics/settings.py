80c2df6bea7660100a153ea9e9834f163446e194
Austin Rouse <austin.rouse@ledcultivation.com>
2025-03-04 00:08:31 -0800

    Add PostgreSQL

--- photonics/settings.py
+++ photonics/settings.py
@@ -1,5 +1,6 @@
 # settings.py
 import os
+import dj_database_url
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -63,13 +64,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "photonics.wsgi.application"
 
-# Database (using SQLite for now)
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
